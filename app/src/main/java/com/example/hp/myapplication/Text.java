ad641bb7b9d5ab6650a419d5de15d1f46f1dc9f8
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2014-12-27 20:26:49 +0000

    Setting OnClickListener to listen to actions of buttons, and modifying text on the User Interface.

--- app/src/main/java/com/example/hp/myapplication/Text.java
+++ app/src/main/java/com/example/hp/myapplication/Text.java
@@ -3,6 +3,9 @@ package com.example.hp.myapplication;
 /** * Created by hp on 17/12/2014. */
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,8 +18,35 @@ public class Text extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.text);
         Button checkButton = (Button) findViewById(R.id.bResults);
-        ToggleButton passToggle = (ToggleButton) findViewById(R.id.tbPassword);
-        EditText input = (EditText) findViewById(R.id.etCommand);
-        TextView display = (TextView) findViewById(R.id.tvResults);
+        final ToggleButton passToggle = (ToggleButton) findViewById(R.id.tbPassword);
+        final EditText input = (EditText) findViewById(R.id.etCommand);
+        final TextView display = (TextView) findViewById(R.id.tvResults);
+
+        passToggle.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+                if (passToggle.isChecked()) {
+                    input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    input.setInputType(InputType.TYPE_CLASS_TEXT);
+                }
+            }
+        });
+
+        checkButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                String checkStr = input.getText().toString();
+                if (checkStr.contentEquals("left")) {
+                    display.setGravity(Gravity.LEFT);
+                } else if (checkStr.contentEquals("center")) {
+                    display.setGravity(Gravity.CENTER);
+                } else if (checkStr.contentEquals("right")) {
+                    display.setGravity(Gravity.RIGHT);
+                } else if (checkStr.contentEquals("blue")) {
+                    
+                }
+            }
+        });
     }
 }
