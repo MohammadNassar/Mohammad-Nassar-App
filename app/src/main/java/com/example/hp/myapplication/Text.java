18efca6d4ad80c45242adbe4aaf0d4e62fc460e3
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2014-12-28 16:00:45 +0000

    Modifying 'Text.java' file by setting text, colour, size and gravity of a TextView component, as well as few enhancements to the code.

--- app/src/main/java/com/example/hp/myapplication/Text.java
+++ app/src/main/java/com/example/hp/myapplication/Text.java
@@ -2,6 +2,7 @@ package com.example.hp.myapplication;
 
 /** * Created by hp on 17/12/2014. */
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.Gravity;
@@ -10,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+import java.util.Random;
 
 public class Text extends Activity {
 
@@ -37,14 +39,23 @@ public class Text extends Activity {
 
             public void onClick(View v) {
                 String checkStr = input.getText().toString();
-                if (checkStr.contentEquals("left")) {
-                    display.setGravity(Gravity.LEFT);
+                display.setText(checkStr);
+                if (checkStr.contentEquals("start")) {
+                    display.setGravity(Gravity.START);
                 } else if (checkStr.contentEquals("center")) {
                     display.setGravity(Gravity.CENTER);
-                } else if (checkStr.contentEquals("right")) {
-                    display.setGravity(Gravity.RIGHT);
+                } else if (checkStr.contentEquals("end")) {
+                    display.setGravity(Gravity.END);
                 } else if (checkStr.contentEquals("blue")) {
-                    
+                    display.setTextColor(Color.BLUE);
+                } else if (checkStr.contentEquals("?")) {
+                    Random random = new Random();
+                    display.setText("?????");
+                    display.setTextSize(random.nextInt(100));
+                    display.setTextColor(Color.rgb(random.nextInt(255), random.nextInt(255), random.nextInt(255)));
+                } else {
+                    display.setText("invalid");
+                    display.setGravity(Gravity.CENTER);
                 }
             }
         });
