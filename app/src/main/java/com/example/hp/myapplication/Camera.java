132b40d16e1082934f78006311efb01226ef10b0
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2015-01-05 23:56:31 +0000

    Getting Data from a different Activity & Finishing Camera and Wallpaper App.

--- app/src/main/java/com/example/hp/myapplication/Camera.java
+++ app/src/main/java/com/example/hp/myapplication/Camera.java
@@ -3,11 +3,13 @@ package com.example.hp.myapplication;
 /** * Created by hp on 30/12/2014. */
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import java.io.IOException;
 
 public class Camera extends Activity implements View.OnClickListener{
 
@@ -16,6 +18,7 @@ public class Camera extends Activity implements View.OnClickListener{
     ImageView iv;
     Intent intent;
     final int cameraData = 0;
+    Bitmap bmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +41,28 @@ public class Camera extends Activity implements View.OnClickListener{
 
         switch (view.getId()) {
             case R.id.bSetWallpaper :
-
+                try {
+                    getApplicationContext().setWallpaper(bmp);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
 
             case R.id.ibTakePic :
                 intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(intent, cameraData);
                 break;
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            bmp = (Bitmap) extras.get("data");
+            iv.setImageBitmap(bmp);
         }
     }
 }
