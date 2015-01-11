5b5d258391ae453396704bb788d59c79d81cf68f
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2015-01-11 22:57:00 +0000

    Setting the Radio to the OnCheckedChangeListener Station.

--- app/src/main/java/com/example/hp/myapplication/Data.java
+++ app/src/main/java/com/example/hp/myapplication/Data.java
@@ -3,12 +3,35 @@ package com.example.hp.myapplication;
 /** * Created by hp on 09/01/2015. */
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Data extends Activity {
+public class Data extends Activity implements View.OnClickListener {
+
+    Button start, startFor;
+    EditText sendET;
+    TextView gotAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.get);
+        initialise();
+    }
+
+    private void initialise() {
+        start = (Button) findViewById(R.id.bSA);
+        startFor = (Button) findViewById(R.id.bSAFR);
+        sendET = (EditText) findViewById(R.id.etSend);
+        gotAnswer = (TextView) findViewById(R.id.tvGot);
+        start.setOnClickListener(this);
+        startFor.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
--- app/src/main/java/com/example/hp/myapplication/OpenedClass.java
+++ app/src/main/java/com/example/hp/myapplication/OpenedClass.java
@@ -3,12 +3,52 @@ package com.example.hp.myapplication;
 /** * Created by hp on 09/01/2015. */
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class OpenedClass extends Activity {
+public class OpenedClass extends Activity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
+
+    TextView question, test;
+    Button returnData;
+    RadioGroup rgSelectionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send);
+        initialise();
+    }
+
+    private void initialise() {
+        question = (TextView) findViewById(R.id.tvQuestion);
+        test = (TextView) findViewById(R.id.tvText);
+        returnData = (Button) findViewById(R.id.bReturn);
+        returnData.setOnClickListener(this);
+        rgSelectionList.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+        switch (i) {
+            case R.id.rBlue :
+
+                break;
+
+            case R.id.rGreen :
+
+                break;
+
+            case R.id.rBoth :
+
+                break;
+        }
     }
 }
