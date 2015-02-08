99e23f9694f5805f4491d7465c1d64f005e17def
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2015-02-08 20:32:34 +0000

    Passing a String between Activities using objects of 'Bundle' and 'Intent'.

--- app/src/main/java/com/example/hp/myapplication/Data.java
+++ app/src/main/java/com/example/hp/myapplication/Data.java
@@ -2,6 +2,7 @@ package com.example.hp.myapplication;
 
 /** * Created by hp on 09/01/2015. */
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,5 +34,23 @@ public class Data extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
 
+        switch (view.getId()) {
+            case R.id.bSA :
+                String bread = sendET.getText().toString();
+                // We'll send data through a Bundle
+                //You can think of a bundle as a basket
+                Bundle basket = new Bundle();
+                //We're going to label the string file: 'bread' with the name: 'Key'.
+                basket.putString("key", bread);
+                //Now we setup an intent, to hold the context of our class: 'Data' and open the class: 'OpenedClass'
+                Intent intent = new Intent(Data.this, OpenedClass.class);
+                //Setup the basket/bundle within out intent
+                intent.putExtras(basket);
+                startActivity(intent);
+                break;
+            case R.id.bSAFR :
+
+                break;
+        }
     }
 }
