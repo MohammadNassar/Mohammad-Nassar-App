e04c8b52e0ec8b4b66adc8cb609519d0f6e3e95b
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2015-02-18 23:01:12 +0000

    Fixing an error with PreferenceActivity. Fix #1. The method 'addPreferencesFromResource(...)'  is deprecated, because as of API 11 an object of PreferenceFragment must be instantiated to load the preference the 'prefs' resource file.

--- app/src/main/java/com/example/hp/myapplication/Prefs.java
+++ app/src/main/java/com/example/hp/myapplication/Prefs.java
@@ -2,13 +2,23 @@ package com.example.hp.myapplication;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 /** * Created by Mohammad Nassar on 15/02/2015. */
 public class Prefs extends PreferenceActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.prefs);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.prefs);
+        }
     }
 }
