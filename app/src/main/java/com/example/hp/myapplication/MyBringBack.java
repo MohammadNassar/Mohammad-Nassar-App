d15df744fbfce236535040443d831609346b65b4
Mohammad Nassar <mohammad.nassar@servicenow.com>
2015-05-13 07:16:38 +0100

    Animating the bitmap by moving the ball downwards repeatedly.

--- app/src/main/java/com/example/hp/myapplication/MyBringBack.java
+++ app/src/main/java/com/example/hp/myapplication/MyBringBack.java
@@ -11,16 +11,25 @@ import android.view.View;
 public class MyBringBack extends View {
 
     Bitmap gBall;
+    float movingY;
 
     public MyBringBack(Context context) {
         super(context);
         gBall = BitmapFactory.decodeResource(getResources(), R.drawable.greenball);
+        movingY = 0;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.WHITE);
-        canvas.drawBitmap(gBall, (canvas.getWidth()/2), 0, null);
+        canvas.drawBitmap(gBall, (canvas.getWidth()/2), movingY, null);
+        if(movingY < canvas.getHeight()) {
+            movingY += 5;
+        }
+        else {
+            movingY = 0;
+        }
+        invalidate();
     }
 }
