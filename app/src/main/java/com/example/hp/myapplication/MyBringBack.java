9a87d41154f4d255fb5be1179f33fe72c7fdc427
Mohammad Nassar <mohammad.nassar@servicenow.com>
2015-05-19 22:29:10 +0100

    Displaying text on the canvas with a custom font using the 'Typeface' object.

--- app/src/main/java/com/example/hp/myapplication/MyBringBack.java
+++ app/src/main/java/com/example/hp/myapplication/MyBringBack.java
@@ -7,6 +7,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.view.View;
 
 /** * Created by Mohammad Nassar on 11/05/2015. */
@@ -14,17 +15,27 @@ public class MyBringBack extends View {
 
     Bitmap gBall;
     float movingY;
+    Typeface font;
 
     public MyBringBack(Context context) {
         super(context);
         gBall = BitmapFactory.decodeResource(getResources(), R.drawable.greenball);
         movingY = 0;
+        font = Typeface.createFromAsset(context.getAssets(), "Artbrush.ttf");
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.WHITE);
+
+        Paint textPaint = new Paint();
+        textPaint.setARGB(85, 175, 50, 200);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setTextSize(35);
+        textPaint.setTypeface(font);
+        canvas.drawText("Mohammad Nassar", getWidth()/2, getHeight()/2, textPaint);
+
         canvas.drawBitmap(gBall, (canvas.getWidth()/2), movingY, null);
         if(movingY < canvas.getHeight()) {
             movingY += 5;
