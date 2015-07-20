ad7b868f7086df613483326e58121674745eea7c
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-07-20 23:40:36 +0100

    Creating a 'Sliding Drawer' container, which enables the user to hide contents from the screen and display them by dragging a handle button.

---
+++ app/src/main/java/com/example/hp/myapplication/Slider.java
@@ -0,0 +1,14 @@
+package com.example.hp.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/** * Created by GBY9C3HL on 20/07/2015. */
+public class Slider extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sliding);
+    }
+}
