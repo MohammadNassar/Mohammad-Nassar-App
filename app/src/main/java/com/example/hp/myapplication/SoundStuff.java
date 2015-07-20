8ecda05ab85c897973504462e2a29218fe7a31aa
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-07-20 22:38:30 +0100

    Utilizing the 'On Long Click' method to run a task on a long click.

--- app/src/main/java/com/example/hp/myapplication/SoundStuff.java
+++ app/src/main/java/com/example/hp/myapplication/SoundStuff.java
@@ -2,15 +2,17 @@ package com.example.hp.myapplication;
 
 import android.app.Activity;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 
 /** * Created by GBY9C3HL on 19/07/2015. */
-public class SoundStuff extends Activity implements View.OnClickListener {
+public class SoundStuff extends Activity implements View.OnClickListener, View.OnLongClickListener {
 
     SoundPool soundPool;
     int bam = 0;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +20,10 @@ public class SoundStuff extends Activity implements View.OnClickListener {
 
         View view = new View(this);
         view.setOnClickListener(this);
+        view.setOnLongClickListener(this);
         setContentView(view);
         soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        mediaPlayer = MediaPlayer.create(this, R.raw.baby_laugh);
 
         //Accessing the sound from a resource
         bam = soundPool.load(this, R.raw.bam_sound, 1);
@@ -33,5 +37,12 @@ public class SoundStuff extends Activity implements View.OnClickListener {
             //      priority [0=lowest priority], loop [0=no loop], rate [1=playback normally]
             soundPool.play(bam, 1, 1, 0, 0, 1);
         }
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        // Play the long sound
+        mediaPlayer.start();
+        return false;
     }
 }
