c16e31a9fa1c819340f675414141976f912466a1
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-08-17 23:52:08 +0100

    Setting up the 'TabHost' in Java to get the correct functioning of tabs, as well as the appropriate look and feel.

--- app/src/main/java/com/example/hp/myapplication/Tabs.java
+++ app/src/main/java/com/example/hp/myapplication/Tabs.java
@@ -2,6 +2,8 @@ package com.example.hp.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 /*** Created by GBY9C3HL on 12/08/2015.*/
 public class Tabs extends Activity {
@@ -10,5 +12,23 @@ public class Tabs extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tabs);
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        TabSpec tabSpecs = tabHost.newTabSpec("tag1");
+        tabSpecs.setContent(R.id.tab1);
+        tabSpecs.setIndicator("Stop Watch");
+        tabHost.addTab(tabSpecs);
+
+        tabSpecs = tabHost.newTabSpec("tag2");
+        tabSpecs.setContent(R.id.tab2);
+        tabSpecs.setIndicator("Tab 2");
+        tabHost.addTab(tabSpecs);
+
+        tabSpecs = tabHost.newTabSpec("tag3");
+        tabSpecs.setContent(R.id.tab3);
+        tabSpecs.setIndicator("Add a Tab");
+        tabHost.addTab(tabSpecs);
     }
 }
