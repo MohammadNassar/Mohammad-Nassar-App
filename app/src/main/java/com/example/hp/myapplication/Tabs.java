9ffc939304ceaec67bcbd7575604e87e4fb3ab9c
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-08-18 23:59:07 +0100

    Creating new Tabs in Java.

--- app/src/main/java/com/example/hp/myapplication/Tabs.java
+++ app/src/main/java/com/example/hp/myapplication/Tabs.java
@@ -1,19 +1,29 @@
 package com.example.hp.myapplication;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
 /*** Created by GBY9C3HL on 12/08/2015.*/
-public class Tabs extends Activity {
+public class Tabs extends Activity implements View.OnClickListener{
+
+    TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tabs);
 
-        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost = (TabHost) findViewById(R.id.tabHost);
+        Button newTab = (Button) findViewById(R.id.bAddTab);
+        Button bStart = (Button) findViewById(R.id.bStartWatch);
+        Button bStop = (Button) findViewById(R.id.bStopWatch);
+        newTab.setOnClickListener(this);
         tabHost.setup();
 
         TabSpec tabSpecs = tabHost.newTabSpec("tag1");
@@ -30,5 +40,36 @@ public class Tabs extends Activity {
         tabSpecs.setContent(R.id.tab3);
         tabSpecs.setIndicator("Add a Tab");
         tabHost.addTab(tabSpecs);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.bAddTab:
+
+                TabSpec theSpec = tabHost.newTabSpec("tag1");
+                theSpec.setContent(new TabHost.TabContentFactory() {
+
+                    @Override
+                    public View createTabContent(String tag) {
+                        TextView text = new TextView(Tabs.this);
+                        text.setText("You have created a new Tab!");
+                        return (text);
+                    }
+                });
+                theSpec.setIndicator("New");
+                tabHost.addTab(theSpec);
+
+                break;
+
+            case R.id.bStartWatch:
+
+                break;
+
+            case R.id.bStopWatch:
+
+                break;
+        }
     }
 }
