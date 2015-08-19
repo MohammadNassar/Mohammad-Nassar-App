38a1f3c4b62a495a5713457ef4c1978c649ca7c9
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-08-19 23:59:47 +0100

    Creating the 'Stop Watch' - (Getting the Time from the System).

--- app/src/main/java/com/example/hp/myapplication/Tabs.java
+++ app/src/main/java/com/example/hp/myapplication/Tabs.java
@@ -13,6 +13,8 @@ import android.widget.TextView;
 public class Tabs extends Activity implements View.OnClickListener{
 
     TabHost tabHost;
+    TextView showResults;
+    long start, stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +22,16 @@ public class Tabs extends Activity implements View.OnClickListener{
         setContentView(R.layout.tabs);
 
         tabHost = (TabHost) findViewById(R.id.tabHost);
+
         Button newTab = (Button) findViewById(R.id.bAddTab);
         Button bStart = (Button) findViewById(R.id.bStartWatch);
         Button bStop = (Button) findViewById(R.id.bStopWatch);
+        showResults = (TextView) findViewById(R.id.tvShowResults);
+
         newTab.setOnClickListener(this);
+        bStart.setOnClickListener(this);
+        bStop.setOnClickListener(this);
+
         tabHost.setup();
 
         TabSpec tabSpecs = tabHost.newTabSpec("tag1");
@@ -40,6 +48,8 @@ public class Tabs extends Activity implements View.OnClickListener{
         tabSpecs.setContent(R.id.tab3);
         tabSpecs.setIndicator("Add a Tab");
         tabHost.addTab(tabSpecs);
+
+        start = 0;
     }
 
     @Override
@@ -65,9 +75,18 @@ public class Tabs extends Activity implements View.OnClickListener{
 
             case R.id.bStartWatch:
 
+                start = System.currentTimeMillis();
+
                 break;
 
             case R.id.bStopWatch:
+
+                stop = System.currentTimeMillis();
+
+                if (start != 0) {
+                    long result = stop - start;
+                    showResults.setText(Long.toString(result));
+                }
 
                 break;
         }
