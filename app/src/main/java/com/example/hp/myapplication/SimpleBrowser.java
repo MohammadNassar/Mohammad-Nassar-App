8168155d790608903c654bd2784d8a40f3f530df
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-08-24 23:57:55 +0100

    Fixing the issue of the 'simplebrowser' XML layout not being displayed and app crashing.

--- app/src/main/java/com/example/hp/myapplication/SimpleBrowser.java
+++ app/src/main/java/com/example/hp/myapplication/SimpleBrowser.java
@@ -10,6 +10,7 @@ public class SimpleBrowser extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.simplebrowser);
 
         WebView webView = (WebView) findViewById(R.id.wvBrowser);
         webView.loadUrl("http://www.amrkhaled.net");
