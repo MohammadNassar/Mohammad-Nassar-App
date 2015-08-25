020078d084b9424dff0f2c7ad408b3468d0ba7c0
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-08-26 00:23:27 +0100

    Overriding the default behaviour of viewing URLs in the default browser application, and instead, viewing URLs in my WebView.

--- app/src/main/java/com/example/hp/myapplication/SimpleBrowser.java
+++ app/src/main/java/com/example/hp/myapplication/SimpleBrowser.java
@@ -3,6 +3,7 @@ package com.example.hp.myapplication;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /*** Created by GBY9C3HL on 23/08/2015. */
 public class SimpleBrowser extends Activity {
@@ -13,6 +14,13 @@ public class SimpleBrowser extends Activity {
         setContentView(R.layout.simplebrowser);
 
         WebView webView = (WebView) findViewById(R.id.wvBrowser);
+        // View URLs through my WebView; not through the default browser.
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+        });
         webView.loadUrl("http://www.amrkhaled.net");
     }
 }
