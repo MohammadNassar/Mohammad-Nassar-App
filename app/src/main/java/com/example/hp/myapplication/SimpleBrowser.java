e885626e6ff617922c8c247e8214343ce80dd36a
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-08-26 23:03:48 +0100

    Calling and using navigation methods of the 'WebView' object.

--- app/src/main/java/com/example/hp/myapplication/SimpleBrowser.java
+++ app/src/main/java/com/example/hp/myapplication/SimpleBrowser.java
@@ -1,19 +1,26 @@
 package com.example.hp.myapplication;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
 
 /*** Created by GBY9C3HL on 23/08/2015. */
-public class SimpleBrowser extends Activity {
+public class SimpleBrowser extends Activity implements View.OnClickListener {
+
+    WebView webView;
+    EditText url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.simplebrowser);
 
-        WebView webView = (WebView) findViewById(R.id.wvBrowser);
+        webView = (WebView) findViewById(R.id.wvBrowser);
         // View URLs through my WebView; not through the default browser.
         webView.setWebViewClient(new WebViewClient() {
             @Override
@@ -22,5 +29,43 @@ public class SimpleBrowser extends Activity {
             }
         });
         webView.loadUrl("http://www.amrkhaled.net");
+
+        Button go = (Button) findViewById(R.id.bGo);
+        Button back = (Button) findViewById(R.id.bBack);
+        Button forward = (Button) findViewById(R.id.bForward);
+        Button refresh = (Button) findViewById(R.id.bRefresh);
+        Button clearHistory = (Button) findViewById(R.id.bClear);
+        url = (EditText) findViewById(R.id.etURL);
+
+        go.setOnClickListener(this);
+        back.setOnClickListener(this);
+        forward.setOnClickListener(this);
+        refresh.setOnClickListener(this);
+        clearHistory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.bGo :
+                String urlToVisit = url.getText().toString();
+                webView.loadUrl(urlToVisit);
+                break;
+            case R.id.bBack :
+                if (webView.canGoBack())
+                    webView.goBack();
+                break;
+            case R.id.bForward :
+                if (webView.canGoForward())
+                    webView.goForward();
+                break;
+            case R.id.bRefresh :
+                webView.reload();
+                break;
+            case R.id.bClear :
+                webView.clearHistory();
+                break;
+        }
     }
 }
