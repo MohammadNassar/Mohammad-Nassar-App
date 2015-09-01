3c106c7167575341219e73338d0140324d55339f
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-09-01 21:27:22 +0100

    Embedding a 'ViewFlipper' element to flip through contents displayed on the screen.

---
+++ app/src/main/java/com/example/hp/myapplication/Flipper.java
@@ -0,0 +1,26 @@
+package com.example.hp.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ViewFlipper;
+
+/*** Created by GBY9C3HL on 01/09/2015. */
+public class Flipper extends Activity implements View.OnClickListener {
+
+    ViewFlipper flipper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flipper);
+
+        flipper = (ViewFlipper) findViewById(R.id.viewFlipper);
+        flipper.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        flipper.showNext();
+    }
+}
