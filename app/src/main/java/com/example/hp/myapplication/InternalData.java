0d9311d54dd96a22a0f278d33b8224b86f83258f
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-09-09 23:59:40 +0100

    Beginning use of: 'File Output Stream for Internal Storage', which is similar to 'Shared Preferences'.

---
+++ app/src/main/java/com/example/hp/myapplication/InternalData.java
@@ -0,0 +1,48 @@
+package com.example.hp.myapplication;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/** * Created by GBY9C3HL on 08/09/2015. */
+public class InternalData extends Activity implements View.OnClickListener {
+
+    EditText sharedData;
+    TextView dataResults;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sharedpreferences);
+        initVars();
+    }
+
+    private void initVars() {
+
+        Button save = (Button) findViewById(R.id.bSave);
+        Button load = (Button) findViewById(R.id.bLoad);
+        sharedData = (EditText) findViewById(R.id.etSharedPrefs);
+        dataResults = (TextView) findViewById(R.id.tvLoadSharedPrefs);
+        save.setOnClickListener(this);
+        load.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.bSave :
+                
+                break;
+
+            case R.id.bLoad :
+
+                break;
+        }
+    }
+}
