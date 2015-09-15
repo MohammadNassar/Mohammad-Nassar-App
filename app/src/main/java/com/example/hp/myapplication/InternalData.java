1de56dd3fe7562b85cbef195ed8c00a0444b4b77
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-09-15 23:40:01 +0100

    Writing and saving data to 'FileOutputStream'.

--- app/src/main/java/com/example/hp/myapplication/InternalData.java
+++ app/src/main/java/com/example/hp/myapplication/InternalData.java
@@ -1,6 +1,7 @@
 package com.example.hp.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +9,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /** * Created by Mohammad Said Nassar on 08/09/2015. */
 public class InternalData extends Activity implements View.OnClickListener {
 
     EditText sharedData;
     TextView dataResults;
+    FileOutputStream fos;
+    String filename = "InternalString";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,15 @@ public class InternalData extends Activity implements View.OnClickListener {
         dataResults = (TextView) findViewById(R.id.tvLoadSharedPrefs);
         save.setOnClickListener(this);
         load.setOnClickListener(this);
+        try {
+            // Creating our FileOutputStream using reference of the filename, writing data to it privately and finally closing it
+            fos = openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -37,7 +54,28 @@ public class InternalData extends Activity implements View.OnClickListener {
         switch (view.getId()) {
 
             case R.id.bSave :
-                
+                String data = sharedData.getText().toString();
+                // Saving data via file
+                // Way no. 1 :
+                /*File file  = new File(filename);
+                try {
+                    fos = new FileOutputStream(file);
+                    // Write some data to the file
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+                // Way no. 1 :
+                try {
+                    fos = openFileOutput(filename, Context.MODE_PRIVATE);
+                    fos.write(data.getBytes());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
 
             case R.id.bLoad :
