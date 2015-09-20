a15fb0cb3ea751dcd7db3c286a7001ea399bb564
Mohammad Said Nassar <mnassar@uk.ibm.com>
2015-09-20 01:08:57 +0100

    Using a Spinners and an ArrayAdapter. Also using the 'OnItemSelected' method and referring to public File Directories. Moreover, toggling Visibility of a component.

--- app/src/main/java/com/example/hp/myapplication/ExternalData.java
+++ app/src/main/java/com/example/hp/myapplication/ExternalData.java
@@ -6,16 +6,24 @@ import android.os.Environment;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.io.File;
 
 /*** Created by GBY9C3HL on 20/09/2015. */
-public class ExternalData extends Activity {
+public class ExternalData extends Activity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
-    private TextView canRead, canWrite;
-    private String state;
+    TextView canRead, canWrite;
+    String state;
+    boolean canR, canW;
+    Spinner spinner;
+    String[] paths = {"Music", "Pictures", "Download"};
+    File path = null;
+    EditText saveFile;
+    Button confirm, save;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +35,69 @@ public class ExternalData extends Activity {
         canRead.setText("false");
         canWrite.setText("false");
 
+        confirm = (Button) findViewById(R.id.bConfirmSaveAs);
+        save = (Button) findViewById(R.id.bSaveFile);
+        confirm.setOnClickListener(this);
+        save.setOnClickListener(this);
+
         state = Environment.getExternalStorageState();
         if (state.equals(Environment.MEDIA_MOUNTED)) {
             // Read and write
             canRead.setText("true");
             canWrite.setText("true");
+            canR = canW = true;
         }
         else if (state.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
             // Read only (cannot write)
             canRead.setText("true");
             canWrite.setText("false");
+            canR = true;
+            canW = false;
         }
         else {
             // Can neither read nor write
             canRead.setText("false");
             canWrite.setText("false");
+            canR = canW = false;
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, paths);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        int position = spinner.getSelectedItemPosition();
+        switch (position) {
+            case 0:
+                path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC);
+                break;
+            case 1:
+                path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                break;
+            case 2:
+                path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bConfirmSaveAs:
+                save.setVisibility(View.VISIBLE);
+                break;
+            case R.id.bSaveFile:
+
+                break;
         }
     }
 }
